097419051eeccb819d0f308194710d30b6207073
Milan Zaric <zaric.milan11@gmail.com>
2025-11-30 22:25:48 +0100

    Added CategoryUpdateDTO

---
+++ src/main/java/com/milanzaric/fridge/dto/category/CategoryUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.milanzaric.fridge.dto.category;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryUpdateDTO {
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+}
