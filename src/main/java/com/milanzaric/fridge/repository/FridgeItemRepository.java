f5cd4e281c5d5be7a0ab1f596b7d5c852f528cb1
Milan Zaric <zaric.milan11@gmail.com>
2025-12-01 00:21:52 +0100

    Added FridgeItemRepository

--- src/main/java/com/milanzaric/fridge/repository/FridgeItemRepository.java
+++ src/main/java/com/milanzaric/fridge/repository/FridgeItemRepository.java
@@ -5,8 +5,12 @@ import com.milanzaric.fridge.entity.FridgeItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FridgeItemRepository extends JpaRepository<FridgeItem, UUID> {
+    List<FridgeItem> findAllByIsDeletedFalse();
+    Optional<FridgeItem> findByIdAndIsDeletedFalse(UUID id);
 }
